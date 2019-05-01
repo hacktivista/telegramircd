7ba21c90c78281ac8053df7fa6d72cf3c0083bee
E. Bosch <presidev@AT@gmail.com>
2019-05-01 14:05:09 +0200

    Add copyright headers in main source file

--- telegramircd.py
+++ telegramircd.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
+#
+# This file is part of telegramircd project
+# This fork: https://github.com/prsai/telegramircd
+# Upstream: https://github.com/MaskRay/telegramircd
+# (C) Copyright 2016-2019 Fangrui Song
+# (C) Copyright 2018 aconitumnapellus, alecdwm
+# (C) Copyright 2019 E. Bosch
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# telegramircd main source
+
 from configargparse import ArgParser, Namespace
 #from ipdb import set_trace as bp
 from collections import deque
